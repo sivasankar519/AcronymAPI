2d17d83031d4701cc6b7ad953103a3e1b5b361e5
sivasankar519 <sivasankar519@gmail.com>
2015-10-19 16:44:37 -0400

    Initial Commit

---
+++ AcronymsAPI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AcronymsAPI
+//
+//  Created by SIVASANKAR DEVABATHINI on 10/19/15.
+//  Copyright (c) 2015 SIVASANKAR DEVABATHINI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AcronymsAPI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AcronymsAPI
+//
+//  Created by SIVASANKAR DEVABATHINI on 10/19/15.
+//  Copyright (c) 2015 SIVASANKAR DEVABATHINI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
